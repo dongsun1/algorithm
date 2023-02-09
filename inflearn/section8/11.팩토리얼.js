23aeb361e22467c9aa37bbf2c635c1f8a135e133
dongsun <le12352@naver.com>
2023-02-09 17:16:13 +0900

    팩토리얼

---
+++ inflearn/section8/11.팩토리얼.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let answer;
+  const dfs = (n) => {
+    if (n === 1) return 1;
+    else return n * dfs(n - 1);
+  }
+  answer = dfs(n);
+  return answer;
+}
+
+console.log(solution(5));
