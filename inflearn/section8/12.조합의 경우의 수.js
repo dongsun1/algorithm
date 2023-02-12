9dfe1f3b4afc296c455a7b4fe44cafba5cc8599f
dongsun1 <le12352@naver.com>
2023-02-12 21:26:48 +0900

    조합의 경우의 수

---
+++ inflearn/section8/12.조합의 경우의 수.js
@@ -0,0 +1,12 @@
+function solution(n, r) {
+  const check = Array.from(Array(n + 1), () => Array(r + 1).fill(0));
+  const dfs = (n, r) => {
+    if (check[n][r]) return check[n][r];
+    if (r === 0 || n === r) return 1;
+    return (check[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r));
+  };
+
+  return dfs(n, r);
+}
+
+console.log(solution(33, 19));
