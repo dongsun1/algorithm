038b3f1cf99466647d3d05cbe15e44c002f4611f
dongsun <le12352@naver.com>
2023-03-21 13:46:42 +0900

    마법의 엘리베이터

---
+++ Programmers/148653.js
@@ -0,0 +1,17 @@
+function solution(storey) {
+  let answer = Number.MAX_SAFE_INTEGER
+
+  const dfs = (num, counter) => {
+    if (counter >= answer) return;
+    if (num === 0) answer = counter;
+    else {
+      let res = num % 10;
+      dfs(Math.floor(num / 10), counter + res);
+      dfs(Math.floor(num / 10) + 1, counter + 10 - res);
+    }
+  }
+
+  dfs(storey, 0)
+
+  return answer;
+}
